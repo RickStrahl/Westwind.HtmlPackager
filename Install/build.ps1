de23d6618112819743f858335966c65e25f9b206
Rick Strahl <rstrahl@west-wind.com>
2018-08-29 01:00:31 -0700

    v0.1.1

--- Install/build.ps1
+++ Install/build.ps1
@@ -1,3 +1,4 @@
+cd "$PSScriptRoot" 
 $release = '..\HtmlPackager\bin\Release'
 
 remove-item $release\*.pdb
@@ -6,6 +7,6 @@ $windir = $env:windir
 $platform = "v4,$windir\Microsoft.NET\Framework64\v4.0.30319"
 
 # Merge Dlls into single EXE
-.\ilmerge /t:exe /targetplatform:$platform /lib:. /out:..\HtmlPackager.exe $release\HtmlPackager.exe $release\Westwind.HtmlPackager.dll $release\HtmlAgilityPack.dll..\
+.\ilmerge /t:exe /targetplatform:$platform /lib:. /out:..\HtmlPackager.exe $release\HtmlPackager.exe $release\Westwind.HtmlPackager.dll $release\HtmlAgilityPack.dll
 
 remove-item ..\HtmlPackager.pdb
