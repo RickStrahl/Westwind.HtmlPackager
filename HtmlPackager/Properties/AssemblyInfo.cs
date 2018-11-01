3bd52376c0b45d14f03bf9d81482b275e2fd70bc
Rick Strahl <rstrahl@west-wind.com>
2018-10-31 21:07:17 -1000

    v0.1.4

--- HtmlPackager/Properties/AssemblyInfo.cs
+++ HtmlPackager/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.1")]
